1caa6dc4eee34c97b3342bd4f4e4c717de7d3b68
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-20 15:02:39 +0530

    Comments Added

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,39 +1,57 @@
+-- Create a new database named AddressBook
 CREATE DATABASE AddressBook;
-use addressbook;
-create table Contacts
+
+-- Switch to the AddressBook database
+USE addressbook;
+
+-- Create a table named Contacts to store contact information
+CREATE TABLE Contacts
 (
-FirstName varchar(150) not null,
-LastName varchar(150),
-address VARCHAR(200),
-city VARCHAR(150),
-state VARCHAR(150),
-zip INT,
-PhoneNumber VARCHAR(15),
-email VARCHAR(319),
-primary key (email)
+    FirstName VARCHAR(150) NOT NULL,
+    LastName VARCHAR(150),
+    Address VARCHAR(200),
+    City VARCHAR(150),
+    State VARCHAR(150),
+    Zip INT,
+    PhoneNumber VARCHAR(15),
+    Email VARCHAR(319),
+    PRIMARY KEY (Email) -- Define Email as the primary key
 );
 
-INSERT INTO contacts(FirstName,LastName,address,city,state,zip,PhoneNumber,email) values
-    ('a','a','a bazar','a','a',312001,'a','a@gmail.com'),
-    ('sanskar','soni','sadar bazar','chittorgarh','rajasthan',312001,'9887279510','sanskarsoni89@gmail.com');
+-- Insert sample data into the Contacts table
+INSERT INTO Contacts(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email) VALUES
+    ('a', 'a', 'a bazar', 'a', 'a', 312001, 'a', 'a@gmail.com'),
+    ('sanskar', 'soni', 'sadar bazar', 'chittorgarh', 'rajasthan', 312001, '9887279510', 'sanskarsoni89@gmail.com');
 
-SELECT * from contacts;
-UPDATE contacts SET city = "Mumbai" WHERE FirstName = "a";
+-- Select all records from the Contacts table
+SELECT * FROM Contacts;
 
-delete from contacts where FirstName = "sanskar";
+-- Update the city to "Mumbai" where the FirstName is "a"
+UPDATE Contacts SET City = "Mumbai" WHERE FirstName = "a";
 
-Select * from contacts where city="mumbai";
+-- Delete records where the FirstName is "sanskar"
+DELETE FROM Contacts WHERE FirstName = "sanskar";
 
-SELECT count(*) as TotalContact FROM contacts where city != "mumbai";
+-- Select records where the city is "Mumbai"
+SELECT * FROM Contacts WHERE City = "Mumbai";
 
-SELECT * FROM contacts ORDER BY FirstName ASC;
+-- Count the total number of contacts where the city is not "Mumbai"
+SELECT COUNT(*) AS TotalContact FROM Contacts WHERE City != "Mumbai";
 
-ALTER TABLE contacts add (
-    type VARCHAR(15)
-);
-DESCRIBE contacts;
+-- Select all records from the Contacts table and order them by FirstName in ascending order
+SELECT * FROM Contacts ORDER BY FirstName ASC;
 
-UPDATE contacts SET type = "friend" WHERE FirstName = "a";
-UPDATE contacts SET type = "family" WHERE FirstName = "sanskar";
+-- Add a new column named "type" to the Contacts table
+ALTER TABLE Contacts ADD (Type VARCHAR(15));
 
-SELECT count(*) as TotalContact FROM contacts where type != "friend";
+-- Display the structure of the Contacts table
+DESCRIBE Contacts;
+
+-- Update the "type" column to "friend" where the FirstName is "a"
+UPDATE Contacts SET Type = "friend" WHERE FirstName = "a";
+
+-- Update the "type" column to "family" where the FirstName is "sanskar"
+UPDATE Contacts SET Type = "family" WHERE FirstName = "sanskar";
+
+-- Count the total number of contacts where the type is not "friend"
+SELECT COUNT(*) AS TotalContact FROM Contacts WHERE Type != "friend";
