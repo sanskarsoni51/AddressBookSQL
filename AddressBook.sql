bb0317d51c4c2adcff92d174349b5193bbb0d369
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-20 13:38:12 +0530

    Creating a new table in address book

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE AddressBook;
 use addressbook;
+create table Contacts
+(
+FirstName varchar(150) not null,
+LastName varchar(150),
+address VARCHAR(200),
+city VARCHAR(150),
+state VARCHAR(150),
+zip INT,
+PhoneNumber VARCHAR(15),
+email VARCHAR(319),
+primary key (email)
+);
